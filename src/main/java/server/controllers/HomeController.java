c0c84656c7a8b9d0f5b0903c66d127cca3e9cd6c
Theo Victor <theo_victor@live.com>
2018-12-05 19:49:41 -0200

    Initial commit

---
+++ src/main/java/server/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Hello world o/");
+		
+		return "/home/index";
+	}
+
+}
