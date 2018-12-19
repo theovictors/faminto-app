3f6f5f3473d7b9827c09097716672385e0b07448
theovictors <theo_victor@live.com>
2018-12-19 18:54:58 -0200

    Minor changes

--- src/main/java/server/controllers/CardapioController.java
+++ src/main/java/server/controllers/CardapioController.java
@@ -3,17 +3,19 @@ package server.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import server.daos.ProdutoDAO;
 
 @Controller
+@RequestMapping("/cardapio")
 public class CardapioController {
 
 	@Autowired
 	private ProdutoDAO produtoDAO;
-
-	@RequestMapping("/cardapio")
+	
+	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView list() {
 		ModelAndView modelAndView = new ModelAndView("cardapio/list");
 		
